ab38a9b9d901f009ecef0aaf6d88459a49575dba
danu44-1 <danudimas042@gmail.com>
2026-06-02 14:24:21 +0700

    Update pengerjaan pretest

--- praktikum_pbo-Mod11-pretest/src/App.java
+++ praktikum_pbo-Mod11-pretest/src/App.java
@@ -1,5 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+
+        try {
+            System.out.print("Masukkan Angka pembilang : ");
+            int angkaA = in.nextInt();
+
+            System.out.print("Masukkan Angka Penyebut : ");
+            int angkaB = in.nextInt();
+
+            int hasil = angkaA / angkaB;
+            System.out.println("Hasil pembagian : " + hasil);
+        }
+        catch(ArithmeticException e) {
+            System.out.println("Angka tidak terduga!");
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Input salah! coba lagi dengan angka");
+        }
+        finally {
+            in.close();
+            System.out.println("Proses kalkulasi selesai dan memori telah dibersihkan!");
+        }
     }
 }
